8d368cb5c67060b3dabedb5646dbab37dd01c9d2
Andrew Kanarek <akanarek@umich.edu>
2018-09-03 20:45:10 -0400

    add files used for debugging on visual studio

---
+++ BlackjackNodeApp/build/Debug/.deps/Debug/obj.target/addon/blackjack/stats.o.d
@@ -0,0 +1,10 @@
+cmd_Debug/obj.target/addon/blackjack/stats.o := c++ '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' '-DDEBUG' '-D_DEBUG' -I/Users/AndrewKanarek/.node-gyp/6.9.5/include/node -I/Users/AndrewKanarek/.node-gyp/6.9.5/src -I/Users/AndrewKanarek/.node-gyp/6.9.5/deps/uv/include -I/Users/AndrewKanarek/.node-gyp/6.9.5/deps/v8/include  -O0 -gdwarf-2 -mmacosx-version-min=10.7 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++0x -fno-rtti -fno-exceptions -fno-threadsafe-statics -std=c++11 -stdlib=libc++ -MMD -MF ./Debug/.deps/Debug/obj.target/addon/blackjack/stats.o.d.raw   -c -o Debug/obj.target/addon/blackjack/stats.o ../blackjack/stats.cpp
+Debug/obj.target/addon/blackjack/stats.o: ../blackjack/stats.cpp \
+  ../blackjack/deck.h ../blackjack/ref.h ../blackjack/stats.h \
+  ../blackjack/hand.h ../blackjack/card.h
+../blackjack/stats.cpp:
+../blackjack/deck.h:
+../blackjack/ref.h:
+../blackjack/stats.h:
+../blackjack/hand.h:
+../blackjack/card.h:
---
+++ BlackjackNodeApp/build/Debug/.deps/Debug/obj.target/addon/blackjack/statsFast.o.d
@@ -0,0 +1,10 @@
+cmd_Debug/obj.target/addon/blackjack/statsFast.o := c++ '-DNODE_GYP_MODULE_NAME=addon' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' '-DDEBUG' '-D_DEBUG' -I/Users/AndrewKanarek/.node-gyp/6.9.5/include/node -I/Users/AndrewKanarek/.node-gyp/6.9.5/src -I/Users/AndrewKanarek/.node-gyp/6.9.5/deps/uv/include -I/Users/AndrewKanarek/.node-gyp/6.9.5/deps/v8/include  -O0 -gdwarf-2 -mmacosx-version-min=10.7 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++0x -fno-rtti -fno-exceptions -fno-threadsafe-statics -std=c++11 -stdlib=libc++ -MMD -MF ./Debug/.deps/Debug/obj.target/addon/blackjack/statsFast.o.d.raw   -c -o Debug/obj.target/addon/blackjack/statsFast.o ../blackjack/statsFast.cpp
+Debug/obj.target/addon/blackjack/statsFast.o: ../blackjack/statsFast.cpp \
+  ../blackjack/deck.h ../blackjack/ref.h ../blackjack/statsFast.h \
+  ../blackjack/hand.h ../blackjack/card.h
+../blackjack/statsFast.cpp:
+../blackjack/deck.h:
+../blackjack/ref.h:
+../blackjack/statsFast.h:
+../blackjack/hand.h:
+../blackjack/card.h:
